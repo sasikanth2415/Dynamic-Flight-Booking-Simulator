4c9c4272fa169308934002636ab05fef405f14f3
SASIKANTH GABU <gsasank007@gmail.com>
2025-10-22 10:19:00 +0530

    Delete flight_booking_simulator directory

--- flight_booking_simulator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('flight_app.urls')),  # include app urls
-]
